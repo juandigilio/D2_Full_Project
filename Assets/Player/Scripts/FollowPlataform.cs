4ccdd6537ae295390c2d683be940677eabbd73e5
juandigilio <juanignaciodigilio@gmail.com>
2024-05-22 03:03:20 -0300

    Added FollowPlatafor
    script to keep player in moving plataforms

---
+++ Assets/Player/Scripts/FollowPlataform.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlataform : MonoBehaviour
+{
+    private Player player;
+    private Vector3 groundPosition;
+    private Vector3 lastGroundPositon;
+    private int groundID;
+    private int lastGroundID;
+    private float height;
+    Quaternion actualRotation;
+    Quaternion lastRotation;
+
+    void Start()
+    {
+        player = GetComponent<Player>();
+
+        GetObjectHeight();
+    }
+
+    private void OnDrawGizmos()
+    {
+        //playerObj = GetComponent<GameObject>();
+        //Gizmos.DrawSphere(playerObj.transform.position, height / 4.0f);
+    }
+
+    void FixedUpdate()
+    {
+        if (player.isGrounded)
+        {
+            RaycastHit hit;
+
+            if (Physics.SphereCast(transform.position, height / 4.2f, -transform.up, out hit))
+            {
+                GameObject groundedIn = hit.collider.gameObject;
+
+                groundID = groundedIn.GetInstanceID();
+                groundPosition = groundedIn.transform.position;
+                actualRotation = groundedIn.transform.rotation;
+
+                Debug.Log(groundID);
+
+                if (groundID == lastGroundID)
+                {
+                    if (groundPosition != lastGroundPositon)
+                    {
+                        transform.position += groundPosition - lastGroundPositon;
+                    }
+
+                    if (actualRotation != lastRotation)
+                    {
+                        Vector3 newRotation = transform.rotation * (actualRotation.eulerAngles - lastRotation.eulerAngles);
+                        transform.RotateAround(groundedIn.transform.position, Vector3.up, newRotation.y);
+                    }
+                }
+
+                lastGroundID = groundID;
+                lastGroundPositon = groundPosition;
+                lastRotation = actualRotation;
+            }
+        }
+        else if (!player.isGrounded)
+        {
+            lastGroundID = -999;
+            lastGroundPositon = Vector3.zero;
+            lastRotation = Quaternion.Euler(0, 0, 0);
+        }
+    }
+
+    float GetObjectHeight()
+    {
+        CapsuleCollider collider = gameObject.GetComponent<CapsuleCollider>();
+        if (collider != null)
+        {
+            return collider.bounds.size.y;
+        }
+        else
+        {
+            Debug.LogError("El GameObject no tiene un componente Renderer.");
+            return 0.0f;
+        }
+    }
+}
